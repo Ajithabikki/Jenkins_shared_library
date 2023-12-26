a7462c35836136af145b30cc4e3566765783e3f8
Ajithabikki <devopspractice1011@gmail.com>
2023-12-26 10:03:51 -0500

    updated checkout git

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,4 +1,8 @@
 def call(String gitUrl, String gitBranch) {
-    checkout scm: [$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Ajithabikki/Youtube-clone-app.git']]]
-
+    checkout scm: [
+        $class: 'GitSCM', 
+        branches: [[name: "*/${main}"]], 
+        extensions: [], 
+        userRemoteConfigs: [[url: gitUrl]]
+    ]
 }
