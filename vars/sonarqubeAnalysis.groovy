a8fa12bf21edddd8473aedb0a33c0576d7230c01
Ajithabikki <devopspractice1011@gmail.com>
2023-12-27 16:10:35 -0500

    changes

---
+++ vars/sonarqubeAnalysis.groovy
@@ -0,0 +1,9 @@
+def call() {
+    withSonarQubeEnv('sonar-server') {
+                        sh ''' ${SCANNER_HOME}/bin/sonar-scanner \
+                        -Dsonar.projectName=YouTube \
+                        -Dsonar.projectKey=YouTube 
+        
+                        '''
+                    }
+}
